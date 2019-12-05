07e87486b2db8cc34ce41231e2adb049399f7139
Bashintoba <57928769+Bashintoba@users.noreply.github.com>
2019-12-05 16:41:21 +0700

    HoaDon

---
+++ Do_An/Quan_Li_Nha_Hang/Gui_QuanLi/HoaDoncs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gui_QuanLi
+{
+    public partial class HoaDoncs : UserControl
+    {
+        public HoaDoncs()
+        {
+            InitializeComponent();
+        }
+    }
+}
